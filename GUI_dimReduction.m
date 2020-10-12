a0e1c05d5ae38c1b8510a6e3bae14a035da8e504
Yixiang Wang <yixiang.wang@yale.edu>
2020-10-12 01:23:23 -0400

    Update GUI_dimReduction.m

--- GUI_dimReduction.m
+++ GUI_dimReduction.m
@@ -764,7 +764,7 @@ scatter3(handles.tSNE_axes, curY(:,1),curY(:,2),curY(:,3),[],cmap,'filled');
 colormap(handles.tSNE_axes, 'copper');
 
 cmap = 1:size(curDmap,1);
-if size(curDmap,2) == 3
+if size(curDmap,2) >= 3
     scatter3(handles.Dmap_axes, curDmap(:,1),curDmap(:,2),curDmap(:,3),[],cmap,'filled');
 else
     scatter(handles.Dmap_axes, curDmap(:,1),curDmap(:,2),[],cmap,'filled');
